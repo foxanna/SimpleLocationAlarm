3be72b2c24f1363d5a9f8d16399293add36c9046
foxanna <anna.domashych@gmail.com>
2014-10-03 13:04:46 +0300

    Remove unused constants

--- SimpleLocationAlarm.Droid/Constants.cs
+++ SimpleLocationAlarm.Droid/Constants.cs
@@ -5,13 +5,6 @@ namespace SimpleLocationAlarm.Droid
 {
 	public static class Constants
 	{
-        public const string DatabaseUpdated_Action = "DatabaseUpdated_Action";
-
-		public const string DatabaseService_SendDatabaseState_Action = "DatabaseService_SendDatabaseState_Action";
-		public const string DatabaseService_AddAlarm_Action = "DatabaseService_AddAlarm_Action";
-        public const string DatabaseService_DeleteAlarm_Action = "DatabaseService_DeleteAlarm_Action";
-        public const string DatabaseService_DeleteAll_Action = "DatabaseService_DeleteAll_Action";
-
 		public const string AlarmsData_Extra = "AlarmsData_Extra";
 
         public const string DeveloperEmail = "simple.location.notifications@gmail.com";
