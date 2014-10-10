840b16b13d1cd75b4a2b27f619fa176f2d36efd3
foxanna <anna.domashych@gmail.com>
2014-10-10 15:31:59 +0300

    Try to add geofence

---
+++ SimpleLocationAlarm.Phone/Services/Tasks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace SimpleLocationAlarm.Phone.Services
+{
+    public class GeofenceEnteredTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral _deferral = taskInstance.GetDeferral();
+            _deferral.Complete();
+        }
+
+        private void OnCompleted(IBackgroundTaskRegistration task, BackgroundTaskCompletedEventArgs args)
+        {
+            //var settings = ApplicationData.Current.LocalSettings;
+            //var key = task.TaskId.ToString();
+            //var message = settings.Values[key].ToString();
+            //UpdateUIExampleMethod(message);
+        }
+    }
+}
