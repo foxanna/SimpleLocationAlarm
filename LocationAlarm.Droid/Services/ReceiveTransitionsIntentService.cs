8109cc455cd14d4a8e6f6583e549e0f4db3b6809
Anna Domashych <anna.domashych@gmail.com>
2015-10-14 14:37:19 +0300

    Refactor ReceiveTransitionsIntentService

--- LocationAlarm.Droid/Services/ReceiveTransitionsIntentService.cs
+++ LocationAlarm.Droid/Services/ReceiveTransitionsIntentService.cs
@@ -10,37 +10,34 @@ namespace SimpleLocationAlarm.Droid.Services
     [Service]
     public class ReceiveTransitionsIntentService : IntentService
     {
-        const string TAG = "ReceiveTransitionsIntentService";
+        private const string TAG = "ReceiveTransitionsIntentService";
 
-        protected override void OnHandleIntent(Android.Content.Intent intent)
+        protected override void OnHandleIntent(Intent intent)
         {
             if (LocationClient.HasError(intent))
             {
                 Log.Debug(TAG, "Location Services error");
-                StartActivity(new Intent(this, typeof(AlarmErrorScreen)).AddFlags(ActivityFlags.NewTask));
+                StartActivity(new Intent(this, typeof (AlarmErrorScreen)).AddFlags(ActivityFlags.NewTask));
             }
             else
             {
                 Log.Debug(TAG, "Location Services success");
 
                 var geofences = LocationClient.GetTriggeringGeofences(intent);
-                if (geofences != null && geofences.Count > 0)
-                {
-                    var requestId = geofences[0].RequestId;
+                if (geofences == null || geofences.Count <= 0) return;
+                var requestId = geofences[0].RequestId;
 
-                    var dbManager = new DBManager();
-                    var alarm = dbManager.GetAlarmByGeofenceRequestId(requestId);
+                var dbManager = new DBManager();
+                var alarm = dbManager.GetAlarmByGeofenceRequestId(requestId);
 
-                    if (alarm != null)
-                    {
-                        StartActivity(intent.SetClass(this, typeof(AlarmScreen))
-                            .AddFlags(ActivityFlags.NewTask)
-                            .PutExtra(Constants.AlarmsData_Extra, JsonConvert.SerializeObject(alarm)));
-                        StartService(new Intent(this, typeof(UIWhileRingingIntentService))
-                            .SetAction(UIWhileRingingIntentService.StartAlarmAction)
-                            .PutExtra(Constants.AlarmsData_Extra, JsonConvert.SerializeObject(alarm)));
-                    }
-                }
+                if (alarm == null) return;
+
+                StartActivity(intent.SetClass(this, typeof (AlarmScreen))
+                    .AddFlags(ActivityFlags.NewTask)
+                    .PutExtra(Constants.AlarmsData_Extra, JsonConvert.SerializeObject(alarm)));
+                StartService(new Intent(this, typeof (UIWhileRingingIntentService))
+                    .SetAction(UIWhileRingingIntentService.StartAlarmAction)
+                    .PutExtra(Constants.AlarmsData_Extra, JsonConvert.SerializeObject(alarm)));
             }
         }
     }
