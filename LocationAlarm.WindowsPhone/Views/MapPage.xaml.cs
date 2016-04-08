b333613583ca2b4903ba98fe6ff38354e8834cc2
Anna Domashych <anna.domashych@gmail.com>
2016-04-08 16:19:15 +0300

    Create page  style and header template

--- LocationAlarm.WindowsPhone/Views/MapPage.xaml.cs
+++ LocationAlarm.WindowsPhone/Views/MapPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using LocationAlarm.PCL;
-using LocationAlarm.PCL.Utils;
-using LocationAlarm.PCL.ViewModels;
-using LocationAlarm.WindowsPhone.Common;
 using Windows.Devices.Geolocation;
 using Windows.Devices.Geolocation.Geofencing;
 using Windows.UI.Core;
@@ -12,179 +8,189 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
+using LocationAlarm.PCL;
+using LocationAlarm.PCL.Utils;
+using LocationAlarm.PCL.ViewModels;
+using LocationAlarm.WindowsPhone.Common;
 
 namespace LocationAlarm.WindowsPhone.Views
 {
-	public sealed partial class MapPage : Page
-	{
-		public NavigationHelper NavigationHelper { get; private set; }
+    public sealed partial class MapPage : Page
+    {
+        private readonly Geolocator locator = new Geolocator();
 
-		readonly Geolocator locator = new Geolocator();
+        private GeoboundingBox boxToDisplay;
 
-		public MapPageViewModel ViewModel { get; private set; }
+        private bool isSomeAlarmSelected;
 
-		public MapPage()
-		{
-			ViewModel = IoC.Get<MapPageViewModel>();
+        public MapPage()
+        {
+            ViewModel = IoC.Get<MapPageViewModel>();
 
-			this.InitializeComponent();
+            InitializeComponent();
 
-			this.NavigationCacheMode = NavigationCacheMode.Required;
+            NavigationCacheMode = NavigationCacheMode.Required;
 
-			NavigationHelper = new NavigationHelper(this);
-			NavigationHelper.LoadState += this.NavigationHelper_LoadState;
-			NavigationHelper.SaveState += this.NavigationHelper_SaveState;
-		}
+            NavigationHelper = new NavigationHelper(this);
+            NavigationHelper.LoadState += NavigationHelper_LoadState;
+            NavigationHelper.SaveState += NavigationHelper_SaveState;
+        }
 
-		void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
-		{
-			locator.MovementThreshold = 5;
-			locator.PositionChanged += LocatorPositionChanged;
+        public NavigationHelper NavigationHelper { get; }
 
-			ViewModel.MapZoomChanged += ViewModel_MapZoomChanged;
-			ViewModel.AlarmsChanged += ViewModel_AlarmsChanged;
-			ViewModel.AlarmScreenRequired += ViewModel_AlarmScreenRequired;
-			ViewModel.AddScreenRequired += ViewModel_AddScreenRequired;
+        public MapPageViewModel ViewModel { get; }
 
-			GeofenceMonitor.Current.GeofenceStateChanged += OnGeofenceStateChanged;
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            locator.MovementThreshold = 5;
+            locator.PositionChanged += LocatorPositionChanged;
 
-			ViewModel.OnStart();
-		}
+            ViewModel.MapZoomChanged += ViewModel_MapZoomChanged;
+            ViewModel.AlarmsChanged += ViewModel_AlarmsChanged;
+            ViewModel.AlarmScreenRequired += ViewModel_AlarmScreenRequired;
+            ViewModel.AddScreenRequired += ViewModel_AddScreenRequired;
 
-		void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
-		{
-			ViewModel.OnStop();
+            GeofenceMonitor.Current.GeofenceStateChanged += OnGeofenceStateChanged;
 
-			GeofenceMonitor.Current.GeofenceStateChanged -= OnGeofenceStateChanged;
+            ViewModel.OnStart();
+        }
 
-			locator.PositionChanged -= LocatorPositionChanged;
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+            ViewModel.OnStop();
 
-			ViewModel.AlarmScreenRequired -= ViewModel_AlarmScreenRequired;
-			ViewModel.AddScreenRequired -= ViewModel_AddScreenRequired;
-			ViewModel.AlarmsChanged -= ViewModel_AlarmsChanged;
-			ViewModel.MapZoomChanged -= ViewModel_MapZoomChanged;
-		}
+            GeofenceMonitor.Current.GeofenceStateChanged -= OnGeofenceStateChanged;
 
-		GeoboundingBox boxToDisplay;
+            locator.PositionChanged -= LocatorPositionChanged;
 
-		void ViewModel_MapZoomChanged(object sender, MapZoomChangedEventArgs e)
-		{
-			boxToDisplay = GeoboundingBox.TryCompute(e.Data.Select(location =>
-				new BasicGeoposition() { Latitude = location.Item1, Longitude = location.Item2 }));
+            ViewModel.AlarmScreenRequired -= ViewModel_AlarmScreenRequired;
+            ViewModel.AddScreenRequired -= ViewModel_AddScreenRequired;
+            ViewModel.AlarmsChanged -= ViewModel_AlarmsChanged;
+            ViewModel.MapZoomChanged -= ViewModel_MapZoomChanged;
+        }
 
-			isSomeAlarmSelected = false;
-			ZoomMap(boxToDisplay);
-		}
+        private void ViewModel_MapZoomChanged(object sender, MapZoomChangedEventArgs e)
+        {
+            boxToDisplay = GeoboundingBox.TryCompute(e.Data.Select(location =>
+                new BasicGeoposition {Latitude = location.Item1, Longitude = location.Item2}));
 
-		async void ZoomMap(GeoboundingBox box)
-		{
-			if (box != null)
-				await Map.TrySetViewBoundsAsync(box, null, MapAnimationKind.Default);
-		}
+            isSomeAlarmSelected = false;
+            ZoomMap(boxToDisplay);
+        }
 
-		void LocatorPositionChanged(Geolocator sender, PositionChangedEventArgs args)
-		{
-			Debug.WriteLine("LocatorPositionChanged to {0};{1}",
-				args.Position.Coordinate.Point.Position.Latitude,
-				args.Position.Coordinate.Point.Position.Longitude);
-			ViewModel.MyCurrentLocation = Tuple.Create<double, double>(args.Position.Coordinate.Point.Position.Latitude, args.Position.Coordinate.Point.Position.Longitude);
-		}
+        private async void ZoomMap(GeoboundingBox box)
+        {
+            if (box != null)
+                await Map.TrySetViewBoundsAsync(box, null, MapAnimationKind.Default);
+        }
 
-		#region NavigationHelper registration
+        private void LocatorPositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            Debug.WriteLine("LocatorPositionChanged to {0};{1}",
+                args.Position.Coordinate.Point.Position.Latitude,
+                args.Position.Coordinate.Point.Position.Longitude);
+            ViewModel.MyCurrentLocation = Tuple.Create(args.Position.Coordinate.Point.Position.Latitude,
+                args.Position.Coordinate.Point.Position.Longitude);
+        }
 
-		/// <summary>
-		/// The methods provided in this section are simply used to allow
-		/// NavigationHelper to respond to the page's navigation methods.
-		/// <para>
-		/// Page specific logic should be placed in event handlers for the
-		/// <see cref="NavigationHelper.LoadState"/>
-		/// and <see cref="NavigationHelper.SaveState"/>.
-		/// The navigation parameter is available in the LoadState method
-		/// in addition to page state preserved during an earlier session.
-		/// </para>
-		/// </summary>
-		/// <param name="e">Event data that describes how this page was reached.</param>
-		protected override void OnNavigatedTo(NavigationEventArgs e)
-		{
-			NavigationHelper.OnNavigatedTo(e);
-		}
+        private async void ViewModel_AddScreenRequired(object sender, EventArgs e)
+        {
+            await
+                Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () => { Frame.Navigate(typeof (AddPage), ViewModel.MyCurrentLocation); });
+        }
 
-		protected override void OnNavigatedFrom(NavigationEventArgs e)
-		{
-			NavigationHelper.OnNavigatedFrom(e);
-		}
+        private void Map_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawAlarmsOnMap();
+            ZoomMap(boxToDisplay);
+        }
 
-		#endregion
+        private void StackPanel_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            isSomeAlarmSelected = true;
+            e.Handled = true;
+            ZoomMap(
+                GeoboundingBox.TryCompute(
+                    ((sender as FrameworkElement).DataContext as AlarmItemViewModel).Alarm.GetPointsForCirle()));
+        }
 
-		async void ViewModel_AddScreenRequired(object sender, EventArgs e)
-		{
-			await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-			{
-				Frame.Navigate(typeof(AddPage), ViewModel.MyCurrentLocation);
-			});
-		}
+        private void StackPanel_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement) sender);
+        }
 
-		void Map_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-		{
-			DrawAlarmsOnMap();
-			ZoomMap(boxToDisplay);
-		}
+        private void ViewModel_AlarmsChanged(object sender, EventArgs e)
+        {
+            DrawAlarmsOnMap();
+        }
 
-		bool isSomeAlarmSelected;
-		void StackPanel_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
-		{
-			isSomeAlarmSelected = true;
-			e.Handled = true;
-			ZoomMap(GeoboundingBox.TryCompute(((sender as FrameworkElement).DataContext as AlarmItemViewModel).Alarm.GetPointsForCirle()));
-		}
+        private void DrawAlarmsOnMap()
+        {
+            Map.MapElements.Clear();
 
-		void StackPanel_RightTapped(object sender, Windows.UI.Xaml.Input.RightTappedRoutedEventArgs e)
-		{
-			FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
-		}
+            foreach (var alarm in ViewModel.Alarms.Select(alarmViewModel => alarmViewModel.Alarm))
+            {
+                var mapCircle = new MapPolygon
+                {
+                    StrokeThickness = 1,
+                    StrokeColor = alarm.Enabled ? AlarmColors.ActiveAlarmDarkColor : AlarmColors.InactiveAlarmDarkColor,
+                    FillColor = alarm.Enabled ? AlarmColors.ActiveAlarmLightColor : AlarmColors.InactiveAlarmLightColor,
+                    StrokeDashed = false,
+                    Path = new Geopath(alarm.GetPointsForCirle())
+                };
+                Map.MapElements.Add(mapCircle);
+            }
+        }
 
-		void ViewModel_AlarmsChanged(object sender, EventArgs e)
-		{
-			DrawAlarmsOnMap();
-		}
+        private void Map_MapTapped(MapControl sender, MapInputEventArgs args)
+        {
+            if (isSomeAlarmSelected)
+                ZoomMap(boxToDisplay);
+        }
 
-		void DrawAlarmsOnMap()
-		{
-			Map.MapElements.Clear();
+        private void OnGeofenceStateChanged(GeofenceMonitor sender, object args)
+        {
+            var reports =
+                GeofenceMonitor.Current.ReadReports()
+                    .Where(report => report.NewState == GeofenceState.Entered)
+                    .Select(report => report.Geofence.Id);
+            ViewModel.OnGeofenceInForeground(reports);
+        }
 
-			foreach (var alarm in ViewModel.Alarms.Select(alarmViewModel => alarmViewModel.Alarm))
-			{
-				var mapCircle = new MapPolygon
-				{
-					StrokeThickness = 1,
-					StrokeColor = alarm.Enabled ? AlarmColors.ActiveAlarmDarkColor : AlarmColors.InactiveAlarmDarkColor,
-					FillColor = alarm.Enabled ? AlarmColors.ActiveAlarmLightColor : AlarmColors.InactiveAlarmLightColor,
-					StrokeDashed = false,
-					Path = new Geopath(alarm.GetPointsForCirle()),
-				};
-				Map.MapElements.Add(mapCircle);
-			}
-		}
+        private async void ViewModel_AlarmScreenRequired(object sender, AlarmItemViewModel e)
+        {
+            await
+                Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () => { Frame.Navigate(typeof (ActiveAlarmPage), e); });
+        }
 
-		void Map_MapTapped(MapControl sender, MapInputEventArgs args)
-		{
-			if (isSomeAlarmSelected)
-				ZoomMap(boxToDisplay);
-		}
+        #region NavigationHelper registration
 
-		void OnGeofenceStateChanged(GeofenceMonitor sender, object args)
-		{
-			var reports = GeofenceMonitor.Current.ReadReports().Where(report => report.NewState == GeofenceState.Entered).Select(report => report.Geofence.Id);
-			ViewModel.OnGeofenceInForeground(reports);
-		}
+        /// <summary>
+        ///     The methods provided in this section are simply used to allow
+        ///     NavigationHelper to respond to the page's navigation methods.
+        ///     <para>
+        ///         Page specific logic should be placed in event handlers for the
+        ///         <see cref="NavigationHelper.LoadState" />
+        ///         and <see cref="NavigationHelper.SaveState" />.
+        ///         The navigation parameter is available in the LoadState method
+        ///         in addition to page state preserved during an earlier session.
+        ///     </para>
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            NavigationHelper.OnNavigatedTo(e);
+        }
 
-		async void ViewModel_AlarmScreenRequired(object sender, AlarmItemViewModel e)
-		{
-			await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-			{
-				Frame.Navigate(typeof(ActiveAlarmPage), e);
-			});
-		}
-	}
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            NavigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
 }
---
+++ LocationAlarm.WindowsPhone/Views/PageTitleBar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LocationAlarm.WindowsPhone.Views
+{
+    public sealed partial class PageTitleBar : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title),
+            typeof (string), typeof (PageTitleBar), new PropertyMetadata(null, TitleChanged));
+
+        public PageTitleBar()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string) GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        private static void TitleChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ((PageTitleBar) sender).TitleLabel.Text = (string) args.NewValue;
+        }
+    }
+}
